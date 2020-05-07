fe400369c7efe13b474fbce876bd81418eec6883
DANGQUOCSON <sondq@vng.com.vn>
2020-05-07 17:48:23 +0700

    [update]:change to ext clock,  fix warning.

--- common/videostate.cpp
+++ common/videostate.cpp
@@ -8,7 +8,7 @@ VideoState::VideoState()
 {    
     continue_read_thread = new QWaitCondition;
     show_mode = ShowMode::SHOW_MODE_VIDEO;    
-    av_sync_type = ShowModeClock::AV_SYNC_VIDEO_MASTER;
+    av_sync_type = ShowModeClock::AV_SYNC_EXTERNAL_CLOCK;
     videoq = new PacketQueue;
     useAVFilter = false;
     ic = nullptr;
@@ -132,16 +132,19 @@ double VideoState::getMasterClock()
 
 void VideoState::checkExternalClockSpeed()
 {
-    if (this->video_stream >= 0 && this->videoq->nb_packets <= EXTERNAL_CLOCK_MIN_FRAMES ||
-           this->audio_stream >= 0 && this->audioq.nb_packets <= EXTERNAL_CLOCK_MIN_FRAMES) {
-           setClockSpeed(&this->extclk, FFMAX(EXTERNAL_CLOCK_SPEED_MIN, this->extclk.speed - EXTERNAL_CLOCK_SPEED_STEP));
-       } else if ((this->video_stream < 0 || this->videoq->nb_packets > EXTERNAL_CLOCK_MAX_FRAMES) &&
-                  (this->audio_stream < 0 || this->audioq.nb_packets > EXTERNAL_CLOCK_MAX_FRAMES)) {
-           setClockSpeed(&this->extclk, FFMIN(EXTERNAL_CLOCK_SPEED_MAX, this->extclk.speed + EXTERNAL_CLOCK_SPEED_STEP));
-       } else {
-           double speed = this->extclk.speed;
-           if (speed != 1.0)
-               setClockSpeed(&this->extclk, speed + EXTERNAL_CLOCK_SPEED_STEP * (1.0 - speed) / fabs(1.0 - speed));
+    if ((this->video_stream >= 0 && this->videoq->nb_packets <= EXTERNAL_CLOCK_MIN_FRAMES) ||
+           (this->audio_stream >= 0 && this->audioq.nb_packets <= EXTERNAL_CLOCK_MIN_FRAMES))
+    {
+        setClockSpeed(&this->extclk, FFMAX(EXTERNAL_CLOCK_SPEED_MIN, this->extclk.speed - EXTERNAL_CLOCK_SPEED_STEP));
+    } else if ((this->video_stream < 0 || this->videoq->nb_packets > EXTERNAL_CLOCK_MAX_FRAMES) &&
+                  (this->audio_stream < 0 || this->audioq.nb_packets > EXTERNAL_CLOCK_MAX_FRAMES))
+    {
+        setClockSpeed(&this->extclk, FFMIN(EXTERNAL_CLOCK_SPEED_MAX, this->extclk.speed + EXTERNAL_CLOCK_SPEED_STEP));
+    } else
+    {
+       double speed = this->extclk.speed;
+       if (speed != 1.0)
+           setClockSpeed(&this->extclk, speed + EXTERNAL_CLOCK_SPEED_STEP * (1.0 - speed) / fabs(1.0 - speed));
     }
 }
 
